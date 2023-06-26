bc5c593300f3a2d2e8596a9a7e3d6d6553f36921
Jose Antonio Martinez <antonio@Joses-iMac.local>
2023-06-26 11:27:48 -0500

    agregando cambio

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
         </p>
+        <p>Deployment de app con dominio en Netlify</p>
         <a
           className="App-link"
           href="https://reactjs.org"
